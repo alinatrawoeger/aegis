8351429bb617ca49fb34b20e1b476afd575c244f
Alina Trawöger <alina.trawoeger@gmx.at>
2022-06-04 16:45:58 +0200

    Remove dist folder

--- dist/src/customCode/dtApp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './dynatraceScript';
-import DynatraceWorldmapApp from './dynatraceScript';
-import './utils';
-var app = ReactDOM.createRoot(document.getElementById('app'));
-app.render(React.createElement(DynatraceWorldmapApp));
-//# sourceMappingURL=dtApp.js.map
--- dist/src/customCode/iVolApp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './ivolunteerScript';
-import IVolunteerWorldmapApp from './ivolunteerScript';
-import './utils';
-var app = ReactDOM.createRoot(document.getElementById('app'));
-app.render(React.createElement(IVolunteerWorldmapApp));
-//# sourceMappingURL=iVolApp.js.map
